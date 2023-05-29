96c6b3ec36cb4c7d7613b09f3843050d4ffa4571
Johannes Brandt Nielsen <johannes.brandt.nielsen@gmail.com>
2023-05-29 21:12:12 +0200

    add router for users crud

---
+++ app/routes/users.tsx
@@ -0,0 +1,54 @@
+import type { LinksFunction, LoaderArgs } from "@remix-run/node";
+import { Form, Link, Outlet, useLoaderData } from "@remix-run/react";
+
+import { getUser } from "~/utils/session.server";
+import stylesUrl from "../styles/jokes.css";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: stylesUrl },
+];
+
+export const loader = async ({ request }: LoaderArgs) => {
+  const user = await getUser(request);
+
+  return { user };
+};
+
+export default function UsersRoute() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div className="jokes-layout">
+      <header className="jokes-header">
+        <div className="container">
+          <h1 className="home-link">
+            <Link to="/" title="Remix Jokes" aria-label="Remix Jokes">
+              <span className="logo">🤪</span>
+              <span className="logo-medium">J🤪KES</span>
+            </Link>
+          </h1>
+          {data.user ? (
+            <div className="user-info">
+              <span>{`Hi ${data.user.username}`}</span>
+              <Form action="/logout" method="post">
+                <button type="submit" className="button">
+                  Logout
+                </button>
+              </Form>
+            </div>
+          ) : (
+            <Link to="/login">Login</Link>
+          )}
+        </div>
+      </header>
+      <main className="jokes-main">
+        <div className="container">
+          <div className="jokes-list">Maybe links here?</div>
+          <div className="jokes-outlet">
+            <Outlet />
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
